a256d2eb26fea181f3f0f48c62075ae3908f753c
Siavash Namvar <snsinahub@github.com>
2024-02-22 11:09:33 -0500

    Update CommonFunctions.ps1

--- samples/CommonFunctions.ps1
+++ samples/CommonFunctions.ps1
@@ -1,50 +1,5 @@
-function New-ScopeID {
-    
-    $GetSiteID = $odata.ExecuteMethod("wmi/SMS_Identification.GetSiteId", "{}");
-    
-    $SiteID   = $GetSiteID["SiteID"].StringValue;
-    $SiteID   = $SiteID.Replace("{","").Replace("}","").ToUpper();
-    $ScopeID  = "ScopeId_$($SiteID)";
+$collectionName = 'Your Collection Name'
+$devices = Get-CMDevice -CollectionName $collectionName
 
-    return $ScopeID
-}
-
-function New-Application {
-    param (
-        [string]$Name,
-        [string]$Description
-    )
-
-    Add-Type -Path "$ENV:SMS_ADMIN_UI_PATH\..\Microsoft.ConfigurationManagement.ApplicationManagement.dll";
-    $scopeId = New-ScopeId;
-
-    $objectId = New-Object -TypeName "Microsoft.ConfigurationManagement.ApplicationManagement.ObjectId" -ArgumentList @($scopeId,([System.Guid]::NewGuid().ToString().ToUpper()),1);
- 
-    $app = New-Object -TypeName "Microsoft.ConfigurationManagement.ApplicationManagement.Application" -ArgumentList $objectId;
-    $app.Name = $Name;
-    $app.Title = $Name;
-    $app.Description = $Description;
-    
-    $displayInfo = New-Object -TypeName "Microsoft.ConfigurationManagement.ApplicationManagement.AppDisplayInfo";
-    $displayInfo.Title = $Name;
-    $displayInfo.Description = $Description;
-    $displayInfo.Language = (Get-Culture).Name
-    
-    $app.DisplayInfo.Add($displayInfo);
-
-
-    $appXml = [Microsoft.ConfigurationManagement.ApplicationManagement.Serialization.SccmSerializer]::SerializeToString($app);
-
-    $body = @{
-        "SDMPackageXML" = $appXml;
-    }
-    
-    Invoke-CMPost -odata $odata -body $body -query "wmi/SMS_Application";
-}
-
-Set-Location 'C:\Program Files (x86)\Microsoft Configuration Manager\AdminConsole\bin'
-Import-Module .\ConfigurationManager.psd1
-Set-Location SNS:
-Get-CMDevice
-# $odata = Connect-CMAdminService;
-New-Application -Name "Test" -Description "Description";
+# Output the devices
+$devices | Format-Table Name, LastLogonTimeStamp, ResourceId, ResourceDomainORWorkgroup, ResourceUserName, ResourceDomainORWorkgroup, ResourceId
