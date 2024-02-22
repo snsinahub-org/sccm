c93e704fb9ee82fe3bb89e1bfc3ac00f1a03d9b3
Siavash Namvar <snsinahub@github.com>
2024-02-22 11:12:37 -0500

    Update CommonFunctions.ps1

--- samples/CommonFunctions.ps1
+++ samples/CommonFunctions.ps1
@@ -1,3 +1,6 @@
+Set-Location 'C:\Program Files (x86)\Microsoft Configuration Manager\AdminConsole\bin'
+Import-Module .\ConfigurationManager.psd1
+Set-Location SNS:
 $collectionName = 'Your Collection Name'
 $devices = Get-CMDevice
 
