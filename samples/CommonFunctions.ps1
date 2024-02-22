3adb3d60d184fb44fe3ca4917b0063c5d6682597
Siavash Namvar <snsinahub@github.com>
2024-02-22 11:11:16 -0500

    Update CommonFunctions.ps1

--- samples/CommonFunctions.ps1
+++ samples/CommonFunctions.ps1
@@ -1,5 +1,5 @@
 $collectionName = 'Your Collection Name'
-$devices = Get-CMDevice -CollectionName $collectionName
+$devices = Get-CMDevice
 
 # Output the devices
 $devices | Format-Table Name, LastLogonTimeStamp, ResourceId, ResourceDomainORWorkgroup, ResourceUserName, ResourceDomainORWorkgroup, ResourceId
